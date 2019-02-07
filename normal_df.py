b4e0e26d76d161cf9bc1b45a4bb54524a546923e
dragonaeve <dragonaeve@gmail.com>
2019-02-06 16:37:49 -0800

    Initial commit

---
+++ normal_df.py
@@ -0,0 +1,13 @@
+import math
+def normal_pdf(x, mu=0, sigma=1):
+    ex = -((x-mu)**2)/2*sigma**2
+    return (math.exp(ex)/math.sqrt(2*math.pi*sigma**2))
+
+from matplotlib import pyplot as plt
+xs = [x / 10.0 for x in range(-50, 50)]
+plt.plot(xs, [normal_pdf(x, sigma=1) for x in xs], '-', label='mu=0,sigma=1')
+plt.plot(xs, [normal_pdf(x, sigma=2) for x in xs], '-', label='mu=0,sigma=2')
+plt.plot(xs, [normal_pdf(x, sigma=0.5) for x in xs], '-', label='mu=0,sigma=0.5')
+plt.plot(xs, [normal_pdf(x, sigma=-1) for x in xs], '-', label='mu=0,sigma=-1')
+plt.legend()
+plt.show()
